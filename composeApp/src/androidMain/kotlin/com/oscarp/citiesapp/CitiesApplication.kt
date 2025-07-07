56801cee4b3f55b524f17c12a25a33af14285b09
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-07 17:32:49 -0600

    Update CitiesApplication.kt

--- composeApp/src/androidMain/kotlin/com/oscarp/citiesapp/CitiesApplication.kt
+++ composeApp/src/androidMain/kotlin/com/oscarp/citiesapp/CitiesApplication.kt
@@ -2,13 +2,16 @@ package com.oscarp.citiesapp
 
 import android.app.Application
 import com.oscarp.citiesapp.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
 
 class CitiesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin {
-          //  androidLogger(level = Level.NONE)
-          //  androidContext(androidContext = this@LoginApplication)
+           androidLogger(level = Level.INFO)
+           androidContext(androidContext = this@CitiesApplication)
         }
     }
 }
