91f40eb52a08bb209ac1ef7c27178bdaacaa2619
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-08 18:23:40 -0600

    Remove example unit test and update CityDao imports

--- data/src/commonMain/kotlin/com/oscarp/citiesapp/data/local/dao/CityDao.kt
+++ data/src/commonMain/kotlin/com/oscarp/citiesapp/data/local/dao/CityDao.kt
@@ -1,6 +1,8 @@
 package com.oscarp.citiesapp.data.local.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.oscarp.citiesapp.data.local.entities.CityEntity
 
 @Dao
