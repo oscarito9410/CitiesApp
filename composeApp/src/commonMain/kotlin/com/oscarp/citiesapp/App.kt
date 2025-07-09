fc1daf8c94f7ccad8fd87c5e440b3837c8af3ebe
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-08 18:28:09 -0600

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/oscarp/citiesapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/oscarp/citiesapp/App.kt
@@ -9,7 +9,11 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import citiesapp.composeapp.generated.resources.Res
