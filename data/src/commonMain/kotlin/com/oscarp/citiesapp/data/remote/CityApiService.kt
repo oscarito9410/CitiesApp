ee27be70e1fb08caebb33ceecea4e65095ea44c8
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-10 13:13:32 -0600

    Update CityApiService.kt

--- data/src/commonMain/kotlin/com/oscarp/citiesapp/data/remote/CityApiService.kt
+++ data/src/commonMain/kotlin/com/oscarp/citiesapp/data/remote/CityApiService.kt
@@ -8,7 +8,13 @@ import io.ktor.utils.io.ByteReadChannel
 class CityApiService(private val client: HttpClient) {
     suspend fun fetchCitiesStream(): ByteReadChannel =
         client.get(
-            "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json"
+            "${BASE_URL}/${CITIES_FILE_PATH}"
         )
             .bodyAsChannel()
+
+
+    companion object {
+        private const val BASE_URL = "https://gist.githubusercontent.com/hernan-uala"
+        private const val CITIES_FILE_PATH = "dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json\""
+    }
 }
