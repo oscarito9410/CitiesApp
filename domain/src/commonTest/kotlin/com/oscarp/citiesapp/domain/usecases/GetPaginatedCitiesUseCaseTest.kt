399f808dfc3a49110b69a3f8c1a0d8c9ac9e5800
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-18 00:35:00 -0600

    Update GetPaginatedCitiesUseCaseTest.kt

--- domain/src/commonTest/kotlin/com/oscarp/citiesapp/domain/usecases/GetPaginatedCitiesUseCaseTest.kt
+++ domain/src/commonTest/kotlin/com/oscarp/citiesapp/domain/usecases/GetPaginatedCitiesUseCaseTest.kt
@@ -1,6 +1,8 @@
 package com.oscarp.citiesapp.domain.usecases
 
+import app.cash.paging.PagingData
 import com.oscarp.citiesapp.domain.factories.CitiesPagingSourceFactory
+import com.oscarp.citiesapp.domain.models.City
 import com.oscarp.citiesapp.domain.testdoubles.FakeCitiesPagingSource
 import dev.mokkery.answering.returns
 import dev.mokkery.every
@@ -12,6 +14,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertIs
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class GetPaginatedCitiesUseCaseTest {
@@ -31,5 +34,6 @@ class GetPaginatedCitiesUseCaseTest {
 
         // then
         verify { factory.create(eq("mexico"), eq(false)) }
+        assertIs<PagingData<City>>(result)
     }
 }
