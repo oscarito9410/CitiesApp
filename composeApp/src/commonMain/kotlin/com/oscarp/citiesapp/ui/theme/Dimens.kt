ebdfec65ffafcbff38e9113c103649882a595606
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2025-07-23 12:59:41 -0600

    removed unused spacing constants from Dimens

--- composeApp/src/commonMain/kotlin/com/oscarp/citiesapp/ui/theme/Dimens.kt
+++ composeApp/src/commonMain/kotlin/com/oscarp/citiesapp/ui/theme/Dimens.kt
@@ -3,13 +3,7 @@ package com.oscarp.citiesapp.ui.theme
 import androidx.compose.ui.unit.dp
 
 object Dimens {
-    // Common Spacing
     val spacingXs = 4.dp
     val spacingSmall = 8.dp
-    val spacingMedium = 12.dp
     val spacingLarge = 16.dp
-    val spacingXl = 24.dp
-    val spacingXxl = 32.dp
-    val spacingXxxl = 48.dp
-    val spacingXxxxl = 64.dp
 }
